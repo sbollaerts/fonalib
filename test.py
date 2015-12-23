f81c2ab1dbeaa17cd6904f85c72704a85a82712a
sbollaerts <sbollaerts@hotmail.com>
2015-12-23 11:24:16 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from fonalib import Fona
+
+PHONE_NUMBER = "{Your phone number here}"
+
+FONA_PORT = "/dev/ttyAMA0"
+FONA_SPEED = 115200
+FONA_PIN = 4250
+FONA_VERBOSE = True
+
+fona = Fona(FONA_PORT, FONA_SPEED, FONA_PIN, FONA_VERBOSE)
+if fona.status == Fona.STATUS_IDLE:
+        pass
+	# fona.send(PHONE_NUMBER, "Hello, World!")
+print("+++ END-OF-EXECUTION +++") 
